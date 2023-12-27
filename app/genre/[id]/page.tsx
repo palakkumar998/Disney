ac51045fdba7ddc9b3089a5da9002b5e1789ee5a
Palak <palakkumar1998@gmail.com>
2023-12-27 19:44:54 +0530

    genre page setup/making navigation url

---
+++ app/genre/[id]/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+type Props = {
+    params: {
+        id: string
+    }
+    searchParams: {
+        genre: string
+    }
+}
+
+const GenrePage = ({ params: { id }, searchParams: { genre } }: Props) => {
+
+
+    return (
+        <div>Welcome to the genre page with ID: {id} & name {genre}</div>
+    )
+}
+
+export default GenrePage
